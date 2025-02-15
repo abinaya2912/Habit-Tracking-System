dee0516d059717d9757c8931e8b0255149df38cc
abinaya2912 <126337413+abinaya2912@users.noreply.github.com>
2025-02-15 19:05:49 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output directory is set
+    emptyOutDir: true, // Clears the output directory before building
+  }
 })
+
